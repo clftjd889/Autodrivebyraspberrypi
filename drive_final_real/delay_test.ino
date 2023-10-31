67a09ba9a282c4d969baceb0f5e5c229fc2cb5b8
clftjd889 <83714459+clftjd889@users.noreply.github.com>
2023-10-31 23:41:06 +0900

    Add files via upload

---
+++ drive_final_real/delay_test.ino
@@ -0,0 +1,279 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+Servo myservo;
+int rgtecho = A0;        //오른쪽 초음파센서
+int rgttrig = A1;
+int lftecho = A2;        //왼쪽 초음파센서
+int lfttrig = A3;
+int mtecho = A4;         //정면 초음파센서
+int mttrig = A5;
+int p = 41;       //서보모터 각도
+float spd = 50;   //전진 모터 속도
+float strling = 1;  //후진 모터 속도
+int rasp = 0;    //라즈베리 주행
+int raspr = 0;
+int z = 103; //servo +=
+int pass = 0;
+
+int delay_count = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  Serial2.begin(9600);
+  pinMode(rgttrig, OUTPUT);
+  pinMode(rgtecho, INPUT);
+  pinMode(lfttrig, OUTPUT);
+  pinMode(lftecho, INPUT);
+  pinMode(mttrig, OUTPUT);
+  pinMode(mtecho, INPUT);    
+  pinMode(8, OUTPUT);      //우회전 세기를 pwm 방식으로 제어
+  pinMode(9, OUTPUT);      //좌회전 세기르 pwm 방식으로 제어
+  myservo.attach(12);
+  myservo.write(p);
+
+  
+  digitalWrite(8, strling);
+  analogWrite(9, spd);      //모터 속도 200rpm   
+  
+}
+
+
+
+void loop() {
+  /*
+  digitalWrite(rgttrig, HIGH);                   //오른쪽 초음파센서 거리
+  delayMicroseconds(10);
+  digitalWrite(rgttrig, LOW);
+  int rdist = pulseIn(rgtecho, HIGH)*17/1000;
+  digitalWrite(lfttrig, HIGH);                  //왼쪽 초음파센서
+  delayMicroseconds(10);
+  digitalWrite(lfttrig, LOW);
+  int ldist = pulseIn(lftecho, HIGH)*17/1000;*/
+  digitalWrite(mttrig, HIGH);                   //정면 초음파센서
+  delayMicroseconds(10);
+  digitalWrite(mttrig, LOW);
+  int dist = pulseIn(mtecho, HIGH)*17/1000;
+
+  if(Serial1.available()) {
+    while(1){
+      if(Serial1.available()){
+        Serial.write(Serial1.read());
+        switch (Serial1.read()) {
+          case 'w':          //(위) 정면
+           p = 41;
+           myservo.write(p);
+           break;
+
+         case 'd':         //(오른쪽) 우회전
+           p = 50;
+            myservo.write(p);
+           break;
+
+         case 'a':         //(왼쪾) 좌회전
+           p = 30;
+            myservo.write(p);
+           break;
+
+         case 'h':                       // (세모) 모터 속도 90rpm
+          spd = 90;
+          digitalWrite(8, HIGH);
+          analogWrite(9, spd);
+          Serial.println(spd);
+          break;
+
+         case 's':                   // (아래) 후진 모터 속도 50rpm
+          spd = 50;
+          digitalWrite(8, LOW);
+          analogWrite(9, spd);
+          Serial.println(spd);
+          break;
+
+         case 't':                  // (x) 모터 정지
+          spd = 0;
+          digitalWrite(8, HIGH);
+          analogWrite(9, spd);
+          Serial.println(spd);
+          break;
+         
+       case 'q':                    //(select) 블루투스 탈출
+        goto exit;
+        break;
+        }
+      }
+    }
+  }
+   
+  exit:
+
+  if (dist <5){ 
+    spd = 0;
+    delay(1000);
+    digitalWrite(8, LOW);
+    analogWrite(9, spd);
+    delay(3000);
+  }
+  
+  if(Serial.available()){
+
+    
+    int raspr = Serial.read();   //라즈베리파이에서 읽어오는 값
+
+
+    if(raspr >= 91 and raspr < 94 and delay_count != 2) {
+      delay(1500); 
+      delay_count = 2;
+    }
+
+    else if(raspr >= 94 and raspr <= 96 and delay_count < 1 and delay_count >= -2) {
+      delay(500);
+      delay_count = 1;
+    }
+
+    else if(raspr >= 100 and raspr <= 105 and delay_count != 0) {
+      delay_count = 0;
+    }
+
+    else if(raspr <= 111 and raspr > 108 and delay_count != -2) {
+      delay(1500);
+      delay_count = -2;
+    }
+
+    else if(raspr <= 108 and raspr > 106 and delay_count <= 2 and delay_count > -1) {
+      delay(500);
+      delay_count = -1;
+    }
+    
+    //rasp = raspr - 64;
+    rasp = raspr - 64;
+    
+
+    if(rasp == 0) {
+      rasp = p;
+    }
+
+    if(rasp != 50 and rasp != 55) {
+      p = rasp;
+    }
+
+    else {
+      p = z;
+    }   
+
+
+    
+    if(rasp == 55){ //blue
+      spd = 50;
+      analogWrite(9, spd);
+      delay(10000);
+      spd = 0;
+      analogWrite(9, spd);
+      delay(5000);
+      spd = 50;
+      analogWrite(9, spd);
+
+      
+      /*
+      while(1){
+        delay(500);
+        if(Serial.read
+      }
+      */
+    }
+     if(rasp == 50){ //red
+      pass = 1;
+      spd = 0;
+     }
+
+     if(rasp > z){    //servo+=
+     p+=6;
+     }
+     if(pass == 0){
+      myservo.write(p);    //라즈베리파이에서 읽어온 값을 서보모터로
+     }
+    
+
+    z = rasp;
+  
+  /*
+    spd = (spd / 4) * 3;
+    strling = HIGH;
+    myservo.write(p);
+    digitalWrite(8, strling);
+    analogWrite(9, spd);    //모터 속도 150rpm 
+    
+  if(rdist < 7){      // 오른쪽 장애물이 7cm이내로 오면 서보모터 오른쪽 이동 (각도 정면 기준 6도)
+    p += 6;
+    spd = spd / 3;
+    strling = LOW;
+    myservo.write(p);
+    digitalWrite(8, strling);
+    analogWrite(9, spd);      //모터 속도 90rpm  
+  }
+  if(ldist < 7){      // 왼쪽 장애물이 7cm이내로 오면 서보모터 왼쪽 이동   (각도 정면 기준 -6도)
+    p -= 6;
+    spd = spd / 3;
+    spd2 = 0;
+    myservo.write(p);
+    digitalWrite(8, strling);
+    analogWrite(9, spd);   //모터 속도 90rpm    
+  }
+  if((ldist < 7) && (rdist < 7)){      // 양쪽이 모두 7cm 이내일 때
+    if(ldist < rdist){                   // 왼쪽이 오른쪽보다 더 가까이 있으면 서보모터 왼쪽으로 이동   (각도 정면 기준 -6도)
+      p -= 6;
+      spd = spd / 3;
+      spd2 = 0;
+      myservo.write(p);
+      digitalWrite(8, strling);
+      analogWrite(9, spd);       //모터 속도 90rpm  
+    }
+    if(rdist < ldist){                  // 오른쪽이 왼쪽보다 더 가까이 있으면 서보모터 오른쪽으로 이동    (각도 정면 기준 6도)
+      p += 6;
+      spd = spd / 3;
+      spd2 = 0;
+      myservo.write(p);
+      digitalWrite(8, strling);
+      analogWrite(9, spd);        //모터 속도 90rpm    
+    }
+    if(rdist == ldist){                //7cm 이내 양쪽 거리 같으면 정면(각도 정면)
+    p = 41;
+    spd = spd / 2;
+    strling = 0;
+    digitalWrite(8, strling);
+    analogWrite(9, spd);        //모터 속도 90rpm
+  }
+  
+  if(dist < 7){       //정면에 7cm 이내에 장애물
+    spd = 0;
+    spd2 = HIGH;
+    analogWrite(9, spd);       
+    digitalWrite(8, strling);     // 후진 117rpm
+  }
+  
+  Serial.print("left = ");      //시리얼모니터에 보이게 함
+  Serial.print(ldist);
+  Serial.print("  right = ");
+  Serial.println(rdist);*/
+  Serial.print("angle = ");
+  Serial.println(p); 
+  //Serial.print("dist = ");
+  //Serial.println(dist);
+  Serial.print("spd = ");
+  Serial.print(spd);
+  Serial.print("  strling = ");
+  Serial.println(strling);
+  Serial.print("\n");
+  if (rasp == 53) {
+    spd = 0;
+    
+  }
+
+  else if (rasp != 50 and rasp != 55) {
+    spd = 50;
+  }
+  digitalWrite(8, strling);
+  analogWrite(9, spd);      //모터 속도 200rpm   
+  }
+  pass = 0;
+  
+}
---
+++ drive_final_real/drive2.4.py
@@ -0,0 +1,368 @@
+import numpy as np
+import cv2
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout = None)
+
+cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+
+z = 'g'
+
+count = 0
+
+while(True):
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 0)
+    frame = cv2.flip(frame, 1)
+    
+    blur = cv2.GaussianBlur(frame, (5,5), 0)
+    
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv) 
+    
+    
+    #색깔 영역
+    
+    low_yellow = np.array([8, 50-10, 150-30])
+    up_yellow = np.array([60, 255, 255])
+    
+    low_white = np.array([0, 0, 255-60])
+    up_white = np.array([180, 0+54, 255])
+    
+    low_red = np.array([0, 50, 50])
+    up_red = np.array([20, 255, 255])
+    
+    low_blue = np.array([120-10, 60, 50])
+    up_blue = np.array([120+20, 255, 255-15])
+    
+    yellow_mask = cv2.inRange(hsv, low_yellow, up_yellow)
+    yellow = cv2.bitwise_and(hsv, hsv, mask = yellow_mask)
+    yellow = cv2.cvtColor(yellow, cv2.COLOR_HSV2BGR)
+    
+    white_mask = cv2.inRange(hsv, low_white, up_white)
+    white = cv2.bitwise_and(hsv, hsv, mask = white_mask)
+    white = cv2.cvtColor(white, cv2.COLOR_HSV2BGR)
+    
+    red_mask = cv2.inRange(hsv, low_red, up_red)
+    red = cv2.bitwise_and(hsv, hsv, mask = red_mask)
+    red = cv2.cvtColor(red, cv2.COLOR_HSV2BGR)
+        
+    blue_mask = cv2.inRange(hsv, low_blue, up_blue)
+    blue = cv2.bitwise_and(hsv, hsv, mask = blue_mask)
+    blue = cv2.cvtColor(blue, cv2.COLOR_HSV2BGR)
+    
+    
+    
+    
+    
+    
+    gray_red = cv2.cvtColor(red, cv2.COLOR_BGR2GRAY)
+    
+    gray_blue = cv2.cvtColor(blue, cv2.COLOR_BGR2GRAY)
+    
+    
+    
+    #주황색 및 흰색 영역 합치기
+        
+    yellow_white = cv2.add(yellow, white)
+    
+    gray = cv2.cvtColor(yellow_white, cv2.COLOR_BGR2GRAY)
+    
+    kernel_size_row = 3
+    kernel_size_col = 3
+    kernel = np.ones((3, 3), np.uint8)
+    
+    erosion_gray = cv2.erode(gray, kernel, iterations=1)
+    dilation_gray = cv2.dilate(erosion_gray, kernel, iterations=1)
+    
+    
+
+    # 모서리 검출
+    can = cv2.Canny(dilation_gray, 150, 400, None, 3)
+    
+    erosion_gray_red = cv2.erode(gray_red, kernel, iterations=1)
+    dilation_gray_red = cv2.dilate(erosion_gray_red, kernel, iterations=1)
+    
+    erosion_gray_blue = cv2.erode(gray_blue, kernel, iterations=1)
+    dilation_gray_blue = cv2.dilate(erosion_gray_blue, kernel, iterations=1)
+    
+    can_red = cv2.Canny(dilation_gray_red, 150, 400, None, 3)
+    
+    can_blue = cv2.Canny(dilation_gray_blue, 150, 400, None, 3)
+
+    # 관심 구역 설정
+    height = can.shape[0]
+    rectangle = np.array([[(20, height), (50, 420), (590, 420), (620, height)]])
+    mask = np.zeros_like(can)
+    cv2.fillPoly(mask, rectangle, 255)
+    masked_image = cv2.bitwise_and(can, mask)
+    ccan = cv2.cvtColor(masked_image, cv2.COLOR_GRAY2BGR)
+    
+    rectangle_red = np.array([[(500, height), (500, 0), (640, 0), (640, height)]])
+    mask_red = np.zeros_like(can_red)
+    cv2.fillPoly(mask_red, rectangle_red, 255)
+    masked_image_red = cv2.bitwise_and(can_red, mask_red)
+    #ccan_red = cv2.cvtColor(masked_image_red, cv2.COLOR_GRAY2BGR)
+    
+    rectangle_blue = np.array([[(560, height), (560, 0), (640, 0), (640, height)]])    
+    mask_blue = np.zeros_like(can_blue)
+    cv2.fillPoly(mask_blue, rectangle_blue, 255)
+    masked_image_blue = cv2.bitwise_and(can_blue, mask_blue)
+
+    
+    rectangle_stop_line = np.array([[(220, height), (270, 300), (370, 300), (420, height)]])
+    mask_stop_line = np.zeros_like(white)
+    cv2.fillPoly(mask_stop_line, rectangle_stop_line, 255)
+    masked_image_stop_line = cv2.bitwise_and(white, mask_stop_line)
+    
+
+    # 직선 검출
+    line_arr = cv2.HoughLinesP(masked_image, 1, np.pi / 180, 20, minLineLength=10, maxLineGap=10)
+
+    # line color
+    # color = [0, 0, 255]
+    # thickness = 5
+    # for line in line_arr:
+    #   for x1, y1, x2, y2 in line:
+    #        cv2.line(ccan, (x1, y1), (x2, y2), color, thickness)
+
+    # 중앙을 기준으로 오른쪽, 왼쪽 직선 분리
+    line_R = np.empty((0, 5), int)
+    line_L = np.empty((0, 5), int)
+    if line_arr is not None:
+        line_arr2 = np.empty((len(line_arr), 5), int)
+        for i in range(0, len(line_arr)):
+            temp = 0
+            l = line_arr[i][0]
+            line_arr2[i] = np.append(line_arr[i], np.array((np.arctan2(l[1] - l[3], l[0] - l[2]) * 180) / np.pi))
+            if line_arr2[i][1] > line_arr2[i][3]:
+                temp = line_arr2[i][0], line_arr2[i][1]
+                line_arr2[i][0], line_arr2[i][1] = line_arr2[i][2], line_arr2[i][3]
+                line_arr2[i][2], line_arr2[i][3] = temp
+            if line_arr2[i][0] < 320 and (abs(line_arr2[i][4]) < 170 and abs(line_arr2[i][4]) > 95):
+                line_L = np.append(line_L, line_arr2[i])
+            elif line_arr2[i][0] > 320 and (abs(line_arr2[i][4]) < 170 and abs(line_arr2[i][4]) > 95):
+                line_R = np.append(line_R, line_arr2[i])
+    line_L = line_L.reshape(int(len(line_L) / 5), 5)
+    line_R = line_R.reshape(int(len(line_R) / 5), 5)
+
+    # 중앙과 가까운 오른쪽, 왼쪽 선을 최종 차선으로 인식
+    try:
+        line_L = line_L[line_L[:, 0].argsort()[-1]]
+        degree_L = line_L[4]
+        cv2.line(ccan, (line_L[0], line_L[1]), (line_L[2], line_L[3]), (255, 0, 0), 10, cv2.LINE_AA)
+    except:
+        degree_L = 0
+    try:
+        line_R = line_R[line_R[:, 0].argsort()[0]]
+        degree_R = line_R[4]
+        cv2.line(ccan, (line_R[0], line_R[1]), (line_R[2], line_R[3]), (255, 0, 0), 10, cv2.LINE_AA)
+    except:
+        degree_R = 0
+        
+    l, r = degree_L, degree_R
+    
+    middle = l + r
+    ming = cv2.addWeighted(frame, 1, ccan, 1, 0)
+    
+    cv2.imshow('yellow_white', ming)
+    #cv2.imshow('red', masked_image_red)
+    #cv2.imshow('white', white)3e
+    #cv2.imshow('yellow', yellow)
+    #cv2.imshow('dilation_gray', dilation_gray)
+    
+    #cv2.imshow('ccan', ccan)
+    #cv2.imshow('blue', masked_image_blue)
+    #cv2.imshow('mask', mask) #ROI 확인용
+    
+    #print('left = ', l)
+    #print('Right = ', r)
+    
+    #if middle > 60 or middle < -60:
+    #    count = 0
+    
+    if middle < 50 and middle > -50:
+        count = 0
+    
+    
+    #servo a ~ k -> c - i
+    
+    
+    if (r >= 0 or middle < -20) and (l != 0):
+        if l >= 140:
+            s = 'o'
+        elif l < 140 and l >= 138 :
+            s = 'n'
+        elif l < 138 and l >= 136:
+            s = 'l'
+        elif l < 136 and l >= 130:
+            s = 'k'
+        elif l < 130 and l >= 124:
+            s = 'j'
+        elif l < 124 and l >= 100:
+            s = 'i'
+        elif l < 100 and l > 0:
+            s = 'c'
+        
+        elif l <= -130 :
+            s = ']'
+        elif l > -130 and l <= -128:
+            s = '^'
+        elif l > -128 and l <= -126:
+            s = '_'
+        elif l > -126 and l <= -120:
+            s = '`'
+        elif l > -120 and l <= -114:
+            s = 'a'
+        elif l > -114 and l <= -100:
+            s = 'b'
+        elif l > -100 and l < 0:
+            s = 'i'
+
+        
+    elif l <= 0 or middle > 20:
+        if r <= -140 :
+            s = '\\'
+        elif r > -140 and r <= -138:
+            s = '^'
+        elif r > -138 and r <= -136:
+            s = '_'
+        elif r > -136 and r <= -130:
+            s = '`'
+        elif r > -130 and r <= -124:
+            s = 'a'
+        elif r > -124 and r <= -100:
+            s = 'b'
+        elif r > -100 and r <= 0:
+            s = 'i'
+            
+        elif r >= 130:
+            s = 'n'
+        elif r < 130 and r >= 128:
+            s = 'm'
+        elif r < 128 and r >= 126:
+            s = 'l'
+        elif r < 126 and r >= 120:
+            s = 'k'
+        elif r < 120 and r >= 114:
+            s = 'j'
+        elif r < 114 and r >= 100:
+            s = 'i'
+        elif r < 100 and r >= 0:
+            s = 'c'
+
+    #elif l == 0 and r <= -130:
+    #    s = 'o'
+    #elif r == 0 and l >= 130:
+    #    s = '^'
+    #elif middle > 100:
+    #    s = 'o'        
+    #elif middle < -100:
+    #    s = '^'    
+    elif middle >= -5 and middle <= 5:  #직진        
+        s = 'g'
+        
+    elif middle < -5 and middle >= -8:  #우회전
+        s = 'h'
+    elif middle < -8 and middle >= -13:
+        s = 'i'
+    elif middle < -13 and middle >= -20: #여기까지 직선 주행
+        s = 'j'
+
+    #elif middle < -20 and middle >= -100:
+    #    s = 'n'
+        
+    elif middle > 5 and middle <= 8:  #좌회전
+        s = 'f'
+    elif middle > 8 and middle <= 13:
+        s = 'e'
+    elif middle > 13 and middle <= 20: #여기까지 직선 주행
+        s = 'd'
+
+    #elif middle > 20 and middle <= 100:
+    #    s = '`'
+        
+
+    if l == 0 and r == 0 and count == 0:
+        if z == '\\' or z == ']' or z == '^' or z == '_' or z == '`' or z == 'a' or z == 'b' or z == 'c':
+            s = '\\'
+            
+        elif z == 'k' or z == 'l' or z == 'm' or z == 'n' or z == 'o':
+            s = 'o'
+    
+    elif (l == 0 or r == 0) and (count == 0):
+        if z == '\\' or z == ']':
+            s = '\\'
+            
+        elif z == 'n' or z == 'o':
+            s = 'o'
+            
+            
+    if z == 'n' or z == 'o' or z == '\\' or z == ']':
+        if count != 0:
+            s = z
+        
+    height = can.shape[0]
+    rectangle_left = np.array([[(20, height), (50, 420), (200, 420), (200, height)]])
+    mask_left = np.zeros_like(ccan)
+    cv2.fillPoly(mask_left, rectangle_left, 255)
+    masked_image_left = cv2.bitwise_and(ccan, mask_left)
+    
+    height = can.shape[0]
+    rectangle_right = np.array([[(200, height), (200, 420), (590, 420), (620, height)]])
+    mask_right = np.zeros_like(ccan)
+    cv2.fillPoly(mask_right, rectangle_left, 255)
+    masked_image_right = cv2.bitwise_and(ccan, mask_right)
+        
+    if count == 1:
+        if z == 'n' or z == 'o' or z == '\\' or z == ']':
+            if l != 0 and r != 0:
+                if masked_image_left.any() != 0 and masked_image_right() != 0:
+                    s = 'g'
+                    count = 0
+
+        
+            
+        
+    #if masked_image_stop_line.any() != 0 and masked_image_red.any() != 0:
+    #    if s != z:
+    #        print('red')
+    #    s = 'r'
+        
+    #if masked_image_blue.any() != 0:
+    #    if s != z:
+    #        print('blue')
+    #        s = 'w'
+    
+    if s != z:
+        print('left = ', l)
+        print('Right = ', r)
+        print('middle = ', middle)
+        print('s = ', s)
+        s = s.encode('utf-8')
+        ser.write(s)
+        s = s.decode('utf-8')
+        
+    #print('left = ', l)
+    #print('Right = ', r)
+    #print('middle = ', middle)
+
+
+    z = s
+    
+    if z == 'n' or z == 'o' or z == '\\' or z == ']':
+        count == 1
+
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+
+cv2.destroyAllWindows()
+
+
+
+
+
+
